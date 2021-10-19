84fe6078946e9b804255fae663f995d23058b9e7
Philip Lambok <philiplambok71@gmail.com>
2021-10-19 15:24:11 +0700

    Add spec

--- spec/lib/midtrans_api_spec.rb
+++ spec/lib/midtrans_api_spec.rb
@@ -7,4 +7,21 @@ describe MidtransApi do
     expect(MidtransApi::API_PRODUCTION_URL).not_to be nil
     expect(MidtransApi::API_SANDBOX_URL).not_to be nil
   end
+
+  describe '#configure' do
+    after do
+      # clening up cache test data
+      described_class.configure do |config|
+        config.logger = nil
+      end
+    end
+
+    it 'returns expected custom logger' do
+      custom_logger = Class.new(Object)
+      described_class.configure do |config|
+        config.logger = custom_logger
+      end
+      expect(described_class.configuration.logger).to eq custom_logger
+    end
+  end
 end
